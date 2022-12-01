02b0433d7f18e699374653138c1b53b48d7d6969
Petr Kracík <petrkr@petrkr.net>
2022-12-01 21:46:50 +0100

    Moved Type init to base Compoment

--- dgus/components/__init__.py
+++ dgus/components/__init__.py
@@ -2,20 +2,21 @@
 # Copyright (c) 2022 Petr Kracik
 # Copyright (c) 2022 OctopusLAB
 
+SP_OFFSET_VP = 0x00
+SP_OFFSET_COLOR = 0x03
+
 class Component:
-    def __init__(self, dgus, sp_address):
+    def __init__(self, dgus, sp_address, component):
         self._sp = sp_address
         self._dgus = dgus
-        self._component = None
+        self._vp = self._dgus.read_vp_int16(self._sp + SP_OFFSET_VP)
+        self._component = component(self._dgus, self._vp)
+        print("Initialized {} at address 0x{:02x} with VP at 0x{:02x}".format(self.__class__.__name__, self._sp, self._vp))
+
 
     @property
     def component(self):
         return self._component
-
-
-    #component.setter
-    def component(self, value):
-        self._compoment = value
 
 
     @property
--- dgus/components/textdisplay.py
+++ dgus/components/textdisplay.py
@@ -6,15 +6,9 @@ from . import Component
 from ..types.string import String
 
 
-SP_OFFSET_VP = 0x00
-SP_OFFSET_COLOR = 0x03
-
 class TextDisplay(Component):
     def __init__(self, dgus, sp_address):
-        super().__init__(dgus, sp_address)
-        vp = self._dgus.read_vp_int16(self._sp + SP_OFFSET_VP)
-        self._component = String(self._dgus, vp)
-        print("Initialized TextDisplay at address 0x{:02x} with VP at 0x{:02x}".format(self._sp, self._component._vp))
+        super().__init__(dgus, sp_address, String)
 
 
     def set_color(self, color):
