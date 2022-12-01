335d65b73e1958ad2ae0410ab9a18ff3859e7c60
Petr Kracík <petrkr@petrkr.net>
2022-12-01 21:06:20 +0100

    Implemented text display component

---
+++ dgus/components/textdisplay.py
@@ -0,0 +1,22 @@
+# TFT components
+# Copyright (c) 2022 Petr Kracik
+# Copyright (c) 2022 OctopusLAB
+
+from .string import String
+
+
+SP_OFFSET_VP = 0x00
+SP_OFFSET_COLOR = 0x03
+
+class TextDisplay(String):
+    def __init__(self, dgus, sp_address):
+        self._sp = sp_address
+        self._dgus = dgus
+        self._vp = self._dgus.read_vp_int16(self._sp + SP_OFFSET_VP)
+        print("Initialized TextDisplay at address 0x{:02x} with VP at 0x{:02x}".format(self._sp, self._vp))
+        super().__init__(self._dgus, self._vp)
+
+
+    def set_color(self, color):
+        self._dgus.write_vp_int16(self._sp + SP_OFFSET_COLOR, color)
+
