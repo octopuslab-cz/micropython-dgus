17b1f7c9713c31f9e0fa7571904d48638d7b60ff
Petr Kracík <petrkr@petrkr.net>
2022-12-01 21:38:30 +0100

    TextDispley changed to base component class

--- dgus/components/textdisplay.py
+++ dgus/components/textdisplay.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2022 Petr Kracik
 # Copyright (c) 2022 OctopusLAB
 
+from . import Component
 from ..types.string import String
 
 
@@ -10,13 +11,11 @@ SP_OFFSET_COLOR = 0x03
 
 class TextDisplay(Component):
     def __init__(self, dgus, sp_address):
-        self._sp = sp_address
-        self._dgus = dgus
-        self._vp = self._dgus.read_vp_int16(self._sp + SP_OFFSET_VP)
-        print("Initialized TextDisplay at address 0x{:02x} with VP at 0x{:02x}".format(self._sp, self._vp))
-        super().__init__(self._dgus, self._vp)
+        super().__init__(dgus, sp_address)
+        vp = self._dgus.read_vp_int16(self._sp + SP_OFFSET_VP)
+        self._component = String(self._dgus, vp)
+        print("Initialized TextDisplay at address 0x{:02x} with VP at 0x{:02x}".format(self._sp, self._component._vp))
 
 
     def set_color(self, color):
         self._dgus.write_vp_int16(self._sp + SP_OFFSET_COLOR, color)
-
