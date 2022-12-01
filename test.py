eb9a4e228c522683f37bdf0dff0a569ef7365d66
Petr Kracik <kracik@aarongroup.net>
2022-12-01 10:02:25 +0100

    FIX: test regards components

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 from machine import UART
-from dgus import DGUS, Int16
+
+from dgus import DGUS
+from dgus.components.int import Int16
 
 
 def dgus_receive(payload):
     print("Got data from display")
     print(payload)
-    
+
+
 def component_value(c, val):
     print("Got data from component {}: {}".format(c, val))
 
 
 ut = UART(2, 115200, rx=36, tx=4)
-
 tft = DGUS(ut)
 tft.event_recv_add(dgus_receive)
 
