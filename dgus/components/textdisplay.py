27d7fc2801823dbd9e8642e272fdf0bb3ca893ca
Petr Kracík <petrkr@petrkr.net>
2022-12-01 22:23:20 +0100

    Text display, implement max text length

--- dgus/components/textdisplay.py
+++ dgus/components/textdisplay.py
@@ -7,9 +7,22 @@ from ..types.string import String
 
 
 class TextDisplay(Component):
+    SP_OFFSET_TEXT_LENGTH = 0x0
+
+
     def __init__(self, dgus, sp_address):
         super().__init__(dgus, sp_address, String)
 
 
     def set_color(self, color):
         self._dgus.write_vp_int16(self._sp + self.SP_OFFSET_COLOR, color)
+
+
+    @property
+    def max_length(self):
+        self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_TEXT_LENGTH)
+
+
+    @max_length
+    def max_length(self, value):
+        self._dgus.write_vp_int16(self._sp + self.SP_OFFSET_TEXT_LENGTH, value)
