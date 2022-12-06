c5d6cd4620e0c438199bc6a1c4f0915335161632
Petr Kracík <petrkr@petrkr.net>
2022-12-06 14:36:37 +0100

    Base component, support VP and support no SP address

--- dgus/components/__init__.py
+++ dgus/components/__init__.py
@@ -8,12 +8,18 @@ class Component:
     SP_OFFSET_POS_Y = 0x02
     SP_OFFSET_COLOR = 0x03
 
-    def __init__(self, dgus, sp_address, component):
-        self._sp = sp_address
+    def __init__(self, dgus, sp_address, component, vp_address=None):
+        self._sp = sp_address or 0xFFFF
         self._dgus = dgus
-        self._vp = self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_VP)
-        self._x = self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_POS_X)
-        self._y = self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_POS_Y)
+        if vp_address is None:
+            self._vp = self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_VP)
+        else:
+            self._vp = vp_address
+
+        if self._sp != 0xFFFF:
+            self._x = self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_POS_X)
+            self._y = self._dgus.read_vp_int16(self._sp + self.SP_OFFSET_POS_Y)
+
         self._component = component(self._dgus, self._vp)
         print("Initialized {} at address 0x{:02x} with VP at 0x{:02x}".format(self.__class__.__name__, self._sp, self._vp))
 
@@ -25,11 +31,17 @@ class Component:
 
     @property
     def position(self):
+        if self._sp == 0xFFFF:
+            raise Exception("This component does not have SP address")
+
         return (self._x, self._y)
 
 
     @position.setter
     def position(self, pos):
+        if self._sp == 0xFFFF:
+            raise Exception("This component does not have SP address")
+
         if not self._dgus.write_vp_int16(self._sp + self.SP_OFFSET_POS_X, pos[0]):
             raise Exception("Error while setting X position")
 
