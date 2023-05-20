57049e7f047c11b92cadfdf4c886dde2bbb4714b
Petr Kracík <petrkr@petrkr.net>
2023-05-20 13:47:21 +0200

    DataVariableDisplay supports different datatype

--- dgus/components/datavariablesdisplay.py
+++ dgus/components/datavariablesdisplay.py
@@ -1,14 +1,14 @@
 # TFT components
-# Copyright (c) 2022 Petr Kracik
-# Copyright (c) 2022 OctopusLAB
+# Copyright (c) 2023 Petr Kracik
+# Copyright (c) 2023 OctopusLAB
 
 from . import Component
 from ..types.int import Int16
 
 
 class DataVariablesDisplay(Component):
-    def __init__(self, dgus, sp_address, vp_address = None):
-        super().__init__(dgus, sp_address, Int16, vp_address)
+    def __init__(self, dgus, sp_address, vp_address = None, datatype=Int16):
+        super().__init__(dgus, sp_address, datatype, vp_address)
 
 
     def set_color(self, color):
