5a3d15528121715bc2d5bd95e9559292bbbec121
Petr Kracik <petrkr@petrkr.net>
2026-06-23 21:38:55 +0200

    Fix Float read property

--- dgus/types/float.py
+++ dgus/types/float.py
@@ -8,14 +8,14 @@ from struct import unpack, pack
 class Float(Type):
     @property
     def value(self):
-        data = self._dgus.read_vp(self._vp)
-        unpack(">f", data)[0]
+        data = self._dgus.read_vp(self._vp, 2)
+        return unpack(">f", data)[0]
 
 
     @value.setter
     def value(self, value):
         data = pack(">f", value)
-        return self._dgus.write_vp(self._vp, data)
+        self._dgus.write_vp(self._vp, data)
 
 
     def _on_change(self, data):
