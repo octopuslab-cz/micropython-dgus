cefe5e36f7b2289f61449aa03be2accca3942c27
Petr Kracik <petrkr@petrkr.net>
2026-06-23 22:22:38 +0200

    Moved stuct to int, added unsigned int16

--- dgus/types/int.py
+++ dgus/types/int.py
@@ -3,17 +3,36 @@
 # Copyright (c) 2026 OctopusLAB
 
 from dgus.types import Type
-from struct import unpack
+from struct import unpack, pack
 
 class Int16(Type):
     @property
     def value(self):
-        return self._dgus.read_vp_int16(self._vp)
+        data = self._dgus.read_vp(self._vp, 1)
+        return unpack(">h", data)[0]
 
 
     @value.setter
     def value(self, value):
-        self._dgus.write_vp_int16(self._vp, value)
+        self._dgus.write_vp(pack(">h", value), self._vp, 1)
+
+
+    def _on_change(self, data):
+        val = unpack('>h', data)[0]
+        for f in self._on_change_events:
+            f(self, val)
+
+
+class UInt16(Type):
+    @property
+    def value(self):
+        data = self._dgus.read_vp(self._vp, 1)
+        return unpack(">H", data)[0]
+
+
+    @value.setter
+    def value(self, value):
+        self._dgus.write_vp(pack(">H", value), self._vp, 1)
 
 
     def _on_change(self, data):
